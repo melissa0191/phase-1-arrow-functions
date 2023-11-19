ab80d757ddbe9f82155a4a7c93b0c7d6ae1cdedc
melissavelasquezgreene <melissa0191@outlook.es>
2023-11-19 01:33:31 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,8 @@
+let divide = (parameter1, parameter2) => parameter1/parameter2;
+divide(2000,100); //=> 20
+
+
+const square = (square) => square * square;
+
+const add = (a, b) => a + b;
+add(3,4);
